2ef979d439791f2c156bb7b2a00b7469a685a1f6
h3nrey <pedro.novaes.education@gmail.com>
2022-09-09 09:21:20 -0300

    (part 3) Ground check and fix infinite Jump, also the player material and black lines

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,23 +4,43 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody2D rb;
     private float movementInputDirection;
+
+    private Rigidbody2D rb;
+    private Animator anim;
+
+    private int amountOfJumpsLeft;
+
     private bool isFacingRight = true;
-    [SerializeField] float movementSpeed = 10.0f;
-    [SerializeField] float jumpForce = 16.0f;
+    private bool isWalking = false;
+    private bool isGrounded = true;
+    private bool canJump = true;
+
+    public int amountOfJumps = 1;
+
+    public float movementSpeed = 10.0f;
+    public float jumpForce = 16.0f;
+    public float groundCheckRadius;
+
+    public Transform groundCheck;
+    public LayerMask whatIsGround;
 
     private void Start() {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        amountOfJumpsLeft = amountOfJumps;
     }
 
     private void Update() {
         CheckInput();
         CheckMovementDirection();
+        UpdateAnimations();
+        CheckIfCanJump();
     }
 
     private void FixedUpdate() {
         ApplyMovement();
+        CheckSurroundings();
     }
 
     private void CheckMovementDirection() {
@@ -29,6 +49,18 @@ public class PlayerController : MonoBehaviour
         } else if(!isFacingRight && movementInputDirection > 0) {
             Flip();
         }
+
+        if(rb.velocity.x != 0) {
+            isWalking = true;
+        } else {
+            isWalking = false;
+        }
+    }
+
+    private void UpdateAnimations() {
+        anim.SetBool("isWalking", isWalking);
+        anim.SetBool("isGrounded", isGrounded);
+        anim.SetFloat("yVelocity", rb.velocity.y);
     }
 
     private void CheckInput() {
@@ -39,16 +71,39 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    private void CheckIfCanJump() {
+        if(isGrounded && rb.velocity.y <= 0) {
+            amountOfJumpsLeft = amountOfJumps;
+        } 
+
+        if(amountOfJumpsLeft <= 0) {
+            canJump = false;
+        } else {
+            canJump = true;
+        }
+    }
+
+    private void CheckSurroundings() {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+    }
+
     private void ApplyMovement() {
         rb.velocity = new Vector2(movementSpeed * movementInputDirection, rb.velocity.y);
     }
 
     private void Jump() {
-        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        if(canJump) {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            amountOfJumpsLeft--;
+        }
     }
 
     private void Flip() {
         isFacingRight = !isFacingRight;
         transform.Rotate(0.0f, 180.0f, 0.0f);
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
     }
 }
